accc2f41c660cf5b265e6d9581ff8e0523e9cd32
Krendel2034 <ilyasuh18@mail.ru>
2025-11-10 16:45:46 +0000

    1

---
+++ less_3.1.py
@@ -0,0 +1,9 @@
+import const 
+import math
+h = 100
+a = 45 
+B = 35
+from math import *
+from const import g
+v =(((g*h)*(math.tan(B)**2))/((2 * math.cos**2)*(1 - (math.tan(B) * math.tan(f)))))**0.5
+print(v)
