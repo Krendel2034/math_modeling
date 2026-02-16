923ed7f9a9df94f25f5247b30ef0d43e05305b66
Krendel2034 <ilyasuh18@mail.ru>
2026-02-16 16:15:19 +0000

    9

---
+++ less_8.py
@@ -0,0 +1,12 @@
+animals = ['fox', 'cat', 'enot']
+ages = [3, 5, 6]
+st_pr = [3, 8, 5]
+
+class Animal:
+    
+    def __init__(self, type, age, st_pr):
+        self.type = type
+        self.age = age
+        self.st_pr = st_pr
+
+    
